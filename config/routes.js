b7feaf80360c003041cc05b414ea527cb531f6dd
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-14 09:28:19 -0700

    functions written, authentication fix,  endpoints working, MVP

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
-const { authenticate } = require('../auth/authenticate');
+const jwt = require('jsonwebtoken');
+const { authenticate, jwtKey } = require('../auth/authenticate');
+const Users = require('../database/user-model')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -12,11 +14,44 @@ function register(req, res) {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 12);
   user.password = hash;
-  
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+    res.status(500).json({ message: `Check register function routes.js`, error:`${error}`})
+  })
+
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Check username and password and try again'})
+      }
+    })
+    .catch(error => {
+    res.status(500).json({ message: 'Server error, check login function on routes.js', error: `${error}`})
+  })
+}
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const option = {
+    expiresIn: '1d'
+  };
+  return jwt.sign(payload, jwtKey, option);
 }
 
 function getJokes(req, res) {
